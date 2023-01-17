e2bfc263f89bdb010cc345fbe0b237984a503468
azire <aziretemir228@gmail.com>
2023-01-17 21:10:18 +0600

    1

--- src/entities/base_entity.py
+++ src/entities/base_entity.py
@@ -18,5 +18,42 @@ class Entity(ABC):
         self.x_coord = x_coord
         self.y_coord = y_coord
 
-    def move(self, direction: Direction):
-        if direction == Direction.east
+    def move(self, direction: Direction, ):
+        if self._check(x_coord, y_coord):
+            if direction == Direction.east:
+                self.x_coord += 1
+            if direction == Direction.north:
+                self.y_coord += 1
+            if direction == Direction.west:
+                self.x_coord -= 1
+            if direction == Direction.south:
+                self.y_coord -= 1
+            return True
+        else:
+            print('move is invalid')
+            return False
+
+    def _check_borders(self, x_coord, y_coord):
+        if x_coord > 100 or x_coord < 1 or y_coord < 1 or y_coord > 100:
+            return False
+        return True
+
+    def _check_other_objects(self, x_coord, y_coord):
+        for monster in monsters:
+            if monster.x_coord == x_coord and monster.y_coord == y_coord:
+                return False
+            return True
+
+    def _check(self, x_coord, y_coord):
+        if self._check_borders(x_coord, y_coord) and self._check_other_objects(x_coord, y_coord):
+            return True
+        return False
+
+
+
+# Дописать метод move для базового класса существ.
+# Метод должен принимать елемент enum Direction и объект класса game.
+# Нужно сделать проверки что на целевой точке не стоит другое существо
+# (перебрать всех существ из игры) и проверить что она находится в допустимом диапазоне.
+# После этого если все ок, то сдвинуть существо и вернуть True,
+# иначе вывести на консоль сообщение об ошибке и вернуть False.
