7bc4b97641ace529a6bc33a91d6d9bbf56e8257f
clear <clarence.sky@yandex.ru>
2023-01-17 21:59:07 +0600

    work with player class(1)

--- src/game/game.py
+++ src/game/game.py
@@ -3,8 +3,8 @@ from src.entities.base_entity import Entity
 
 
 class Player(Entity):
-    def __init__(self, x_coord, y_coord):
-        super().__init__(x_coord, y_coord)
+    def __init__(self):
+        super().__init__(self.x_coord, self.y_coord)
         self.hp = 500
         self.x_coord = randrange(100)
         self.y_coord = randrange(100)
@@ -13,7 +13,7 @@ class Game:
     def __init__(self):
         self.entites = []
         self.is_ended = False
-        self.player = Player(self)
+        self.player = Player()
 
     def get_command(self):
         while True:
