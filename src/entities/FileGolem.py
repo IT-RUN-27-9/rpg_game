4a99e8e79d3cee605a3cbd466a14626dd4f22a7f
vladmesh <vladiclav.me@gmail.com>
2023-01-26 20:14:51 +0600

    small fixes

--- src/entities/FileGolem.py
+++ src/entities/FileGolem.py
@@ -6,4 +6,4 @@ from src.incantation.fireball import Fireball
 class FireGolem(Mage, Monster):
     def __init__(self, x_coord, y_coord, game):
         super().__init__(x_coord, y_coord, 100, 50, 25, game)
-        self.incantations = [Fireball]
+        self.incantations = [Fireball()]
--- src/entities/Mage.py
+++ src/entities/Mage.py
@@ -6,7 +6,7 @@ class Mage(Entity):
     def __init__(self, x_coord: int, y_coord: int, hp: int, attack: int, mana: int, game):
         super().__init__(x_coord, y_coord, hp, attack, game)
         self.mana = mana
-        self.incantations = [Heal]
+        self.incantations = [Heal()]
 
     def do_magic(self, target):
         target.hp -= self.incantations
